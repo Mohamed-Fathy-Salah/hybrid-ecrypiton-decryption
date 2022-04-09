70e2f1177607a0c2497a050351d4545fd17dbdd4
Mohamed Fathy <mofasa.peace@gmail.com>
2022-04-09 20:30:51 +0200

    first commit

---
+++ AES.py
@@ -0,0 +1,33 @@
+from Crypto.Cipher import AES
+from secrets import token_bytes
+import RSA
+
+def encrypt(video_file, RSA_public_key_file):
+    key = token_bytes(16)
+    RSA.encrypt(key, RSA_public_key_file)
+
+    model = AES.new(key, AES.MODE_EAX, nonce=b'0')
+
+    with open(video_file, 'rb') as f:
+        plain_text = f.read()
+        f.close();
+
+    encrypted_text = model.encrypt(plain_text)
+
+    with open("{}.enc".format(video_file), 'wb') as f:
+        f.write(encrypted_text)
+        f.close()
+
+def decrypt(encrypted_video_file, AES_key_file, RSA_private_key_file):
+    key = RSA.decrypt(AES_key_file, RSA_private_key_file)
+    model = AES.new(key, AES.MODE_EAX, nonce=b'0')
+
+    with open(encrypted_video_file, 'rb') as f:
+        encrypted_text = f.read()
+    decrypted_text = model.decrypt(encrypted_text)
+
+    decrypted_video_file = encrypted_video_file.removesuffix('.enc')
+
+    with open(decrypted_video_file, 'wb') as f:
+        f.write(decrypted_text)
+
---
+++ RSA.py
@@ -0,0 +1,47 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
+def generate_key():
+    key = RSA.generate(2048)
+
+    public_key = key.publickey().exportKey('PEM')
+    with open("public_key", "wb") as f:
+        f.write(public_key)
+        f.close()
+
+    private_key = key.export_key('PEM')
+    with open("private_key", "wb") as f:
+        f.write(private_key)
+        f.close()
+
+def encrypt(text, key_file):
+    with open(key_file, "r") as f:
+        public_key = f.read()
+        f.close()
+
+    rsa_public_key = RSA.importKey(public_key)
+    rsa_public_key = PKCS1_OAEP.new(rsa_public_key)
+    encrypted_text = rsa_public_key.encrypt(text)
+
+    with open("AES_key.enc", "wb") as f:
+        f.write(encrypted_text)
+        f.close()
+
+def decrypt(text_file, key_file):
+    with open(text_file, "rb") as f:
+        text = f.read()
+        f.close()
+    
+    with open(key_file, "r") as f:
+        private_key = f.read()
+        f.close()
+
+    rsa_private_key = RSA.importKey(private_key)
+    rsa_private_key = PKCS1_OAEP.new(rsa_private_key)
+    decrypted_text = rsa_private_key.decrypt(text)
+
+    with open("{}.dec".format(text_file), "wb") as f:
+        f.write(decrypted_text)
+        f.close()
+
+    return decrypted_text
